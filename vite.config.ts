988afd09a1159686b8267e5f9a7d0af5cfbcb1b2
hesouam <hestuci@gmail.com>
2025-08-09 14:57:35 +0300

    first

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-/// git<reference types="vitest" />
 
 // https://vite.dev/config/
 export default defineConfig({
